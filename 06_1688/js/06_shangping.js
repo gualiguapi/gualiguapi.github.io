2c858c39f8efda0aad64616ede14586dc78f4681
gualiguapi <68582736+gualiguapi@users.noreply.github.com>
2020-07-23 09:04:51 +0800

    Add files via upload

---
+++ 06_1688/js/06_shangping.js
@@ -0,0 +1,12 @@
+
+
+$(function(){
+	
+	$(".header_Business_info").mouseover(function(){
+		$(".binfo_space2").show();
+	});
+	$(".header_Business_info").mouseout(function(){
+		$(".binfo_space2").hide();
+	});
+	
+ });
